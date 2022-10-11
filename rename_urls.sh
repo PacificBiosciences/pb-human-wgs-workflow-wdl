e94c35a3ec9ac138e90377214da0256fac8ec5a7
Venkat Malladi <vmalladi@microsoft.com>
2022-10-11 00:48:56 -0500

    Fix syntax.

--- rename_urls.sh
+++ rename_urls.sh
@@ -16,13 +16,13 @@ echo -e "\tTo change the local and remote URLs, please edit this '$0' script."
 echo " "
 echo " "
 
-if [ $# -eq 1 ] && [ "$1" == "--remote" ]; then
+if [ $# -eq 2 ] && [ "$2" == "--remote" ]; then
 	echo -en "Changing all WDL Github URLs from '${local_user}/${local_repo}' to '${remote_user}/${remote_repo}'..."
 	find . -type f -name "*.wdl" -print0 | \
 		xargs -0 sed -i "s/com\/${local_user}\/${local_repo}/com\/${remote_user}\/${remote_repo}/g"
 	echo "done!"
 
-elif [ $# -eq 1 ] && [ "$1" == "--local" ]; then
+elif [ $# -eq 2 ] && [ "$2" == "--local" ]; then
 	echo -en "Changing all WDL Github URLs from '${remote_user}/${remote_repo}' to '${local_user}/${local_repo}'..."
 	find . -type f -name "*.wdl" -print0 | \
 		xargs -0 sed -i "s/com\/${remote_user}\/${remote_repo}/com\/${local_user}\/${local_repo}/g"
@@ -31,9 +31,9 @@ elif [ $# -eq 1 ] && [ "$1" == "--local" ]; then
 else
 	echo "Usage:"
 	echo " "
-	echo "./rename_urls --remote"
+	echo "./rename_urls.sh local_user --remote"
 	echo -e "\t\tChange all WDL Github URLs from '${local_user}/${local_repo}' to '${remote_user}/${remote_repo}'"
-	echo "./rename_urls --local"
+	echo "./rename_urls.sh local_user --local"
 	echo -e "\t\tChange all WDL Github URLs from '${remote_user}/${remote_repo}' to '${local_user}/${local_repo}'"
 
 fi
